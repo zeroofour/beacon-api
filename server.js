1b7e5d8c9a83a6aa73c5d6c2323257debe7fd6da
zerofour <fynnmaddox1@gmail.com>
2026-07-30 12:24:07 +0200

    add demo page

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 app.use(api);
 setupGitHubWebhook(app);
 setupWebSocket(server);
