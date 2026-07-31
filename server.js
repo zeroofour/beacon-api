0224426967ca0976bd68a640781b2d0f4b7c58d6
zerofour <fynnmaddox1@gmail.com>
2026-07-31 23:04:12 +0200

    fixed github

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
-app.use(api);
 
 setupGitHubWebhook(app);
+app.use(api);
 setupWebSocket(server);
 
 let serverIds = [];
